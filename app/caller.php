c0dc6beab8192dff0efa21ded2a6176a7ee7ee7d
SimonaPetseva <petsevasimona@gmail.com>
2019-02-18 13:27:26 +0000

    Login

--- app/caller.php
+++ app/caller.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Caller extends Model
 {
-    //Each caller may have one or more queries
     public function display_queries(){
         return $this->hasMany(problem_query::class);
     }
